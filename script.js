4bc4b764e574c6eba1ab00822a7784581f8cff7a
Huong <huongnguyen.nth1709@gmail.com>
2023-11-29 20:34:32 +0200

    implement function startNote with audioContext

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const audioContext = new AudioContext();
+
 const NOTE_DETAILS = [
   { note: 'C', key: 'Z', frequency: 261.626 },
   { note: 'Db', key: 'S', frequency: 277.183 },
@@ -43,12 +45,37 @@ function playNotes() {
   NOTE_DETAILS.forEach((n) => {
     const keyElement = document.querySelector(`[data-note="${n.note}"]`);
     keyElement.classList.toggle('active', n.active || false);
+
+    if (n.oscillator != null) {
+      n.oscillator.stop();
+      n.oscillator.disconnect();
+    }
   });
 
   const activeNotes = NOTE_DETAILS.filter((n) => n.active);
+
+  // if have 1 note active, volume is 100%,
+  // if have 2 notes active, volume is 50% each note
+  const gain = 1 / activeNotes.length;
+
   activeNotes.forEach((n) => {
-    startNote(n);
+    startNote(n, gain);
   });
 }
 
-function startNote(noteDetail) {}
+function startNote(noteDetail, gain) {
+  const audioContext = new AudioContext();
+
+  // reduce the volume for each note base on how many notes are active
+  const gainNode = audioContext.createGain();
+  gainNode.gain.value = gain;
+
+  const oscillator = audioContext.createOscillator();
+  oscillator.frequency = noteDetail.frequency;
+  oscillator.type = 'sine';
+
+  oscillator.connect(gainNode).connect(audioContext.destination);
+  oscillator.start();
+
+  noteDetail.oscillator = oscillator;
+}
